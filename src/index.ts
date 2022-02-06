b09d7c394e843446abb8ba5563f040c016d0f342
ryuji <yrejgnl11@163.com>
2022-02-06 23:45:15 +0800

    feat: process request data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 
 function Axios(config: AxiosRequestConfig) {
   processConfig(config)
@@ -9,11 +10,16 @@ function Axios(config: AxiosRequestConfig) {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
+  config.data = transfromRequestData(config)
 }
 
 function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transfromRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 
 export default Axios
